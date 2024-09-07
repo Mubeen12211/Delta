d336078c906faae1b52cd091f02ec4c63cd4e849
Mubeen12211 <168086426+Mubeen12211@users.noreply.github.com>
2024-09-07 20:30:50 +0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install Chrome
+echo "Installing Google Chrome..."
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
+
+echo "Google Chrome installed."
